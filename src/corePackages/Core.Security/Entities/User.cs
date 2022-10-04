cfa411a2160ecab8d01a8a52c71c229eafbca513
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2022-10-04 16:57:16 +0300

    refactor

--- src/corePackages/Core.Security/Entities/User.cs
+++ src/corePackages/Core.Security/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Persistence.Repositories;
 using Core.Security.Enums;
+using System.Linq.Expressions;
 
 namespace Core.Security.Entities;
 
@@ -30,4 +31,39 @@ public class User : Entity
         Status = status;
         AuthenticatorType = authenticatorType;
     }
+}
+
+public enum UserProperties
+{
+    Id,
+    Email,
+    PasswordSalt,
+    PasswordHash,
+    Status,
+    AuthenticatorType
+}
+
+public static class UserPropertiesExtensions
+{
+    public static string GetColumnName(this UserProperties userProperty) => userProperty switch
+    {
+        UserProperties.Id => "Id",
+        UserProperties.Email => "Email",
+        UserProperties.PasswordSalt => "PasswordSalt",
+        UserProperties.PasswordHash => "PasswordHash",
+        UserProperties.Status => "Status",
+        UserProperties.AuthenticatorType => "AuthenticatorType",
+        _ => throw new Exception("property not found int user entity"),
+    };
+
+    public static Expression<Func<User, object?>> GetExpression(this UserProperties userProperty) => userProperty switch
+    {
+        UserProperties.Id => (x => x.Id),
+        UserProperties.Email => (x => x.Email),
+        UserProperties.PasswordSalt => (x => x.PasswordSalt),
+        UserProperties.PasswordHash => (x => x.PasswordHash),
+        UserProperties.Status => (x => x.Status),
+        UserProperties.AuthenticatorType => (x => x.AuthenticatorType),
+        _ => throw new Exception("property not found int user entity"),
+    };
 }
--- src/kodlamayoDevs/Kodlama.io.Devs.Persistence/Configurations/UserConfiguration.cs
+++ src/kodlamayoDevs/Kodlama.io.Devs.Persistence/Configurations/UserConfiguration.cs
@@ -9,13 +9,14 @@ namespace Kodlama.io.Devs.Persistence.Configurations
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).HasColumnName("Id");
-            builder.Property(x => x.Status).HasColumnName("Status");
-            builder.Property(x => x.AuthenticatorType).HasColumnName("AuthenticatorType");
-            builder.Property(x => x.Email).HasColumnName("Email");
-            builder.Property(x => x.PasswordHash).HasColumnName("PasswordHash");
-            builder.Property(x => x.PasswordSalt).HasColumnName("PasswordSalt");
+            builder.HasKey(UserProperties.Id.GetExpression());
+            builder.Property(UserProperties.Id.GetExpression()).HasColumnName(UserProperties.Id.GetColumnName());
+            builder.Property(UserProperties.Status.GetExpression()).HasColumnName(UserProperties.Status.GetColumnName());
+            builder.Property(UserProperties.AuthenticatorType.GetExpression()).HasColumnName(UserProperties.AuthenticatorType.GetColumnName());
+            builder.Property(UserProperties.Email.GetExpression()).HasColumnName(UserProperties.Email.GetColumnName());
+            builder.HasIndex(UserProperties.Email.GetExpression()).IsUnique();
+            builder.Property(UserProperties.PasswordHash.GetExpression()).HasColumnName(UserProperties.PasswordHash.GetColumnName());
+            builder.Property(UserProperties.PasswordSalt.GetExpression()).HasColumnName(UserProperties.PasswordSalt.GetColumnName());
             builder.ToTable(Entities.User.GetTableName());
         }
     }
