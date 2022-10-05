e3a3daab5371dabb7aebfd506acea874ee0cc104
sakirsuleymanoglu <sakirsuleymanoglu34@gmail.com>
2022-10-05 22:11:45 +0300

    refactor

--- src/kodlamayoDevs/Kodlama.io.Devs.Application/Features/UserOperationClaims/Commands/CreateUserOperationClaim/CreateUserOperationClaimCommandHandler.cs
+++ src/kodlamayoDevs/Kodlama.io.Devs.Application/Features/UserOperationClaims/Commands/CreateUserOperationClaim/CreateUserOperationClaimCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Core.Security.Entities;
+using Kodlama.io.Devs.Application.Exceptions.OperationClaims;
 using Kodlama.io.Devs.Application.Exceptions.UserOperationClaims;
+using Kodlama.io.Devs.Application.Exceptions.Users;
 using Kodlama.io.Devs.Application.Services.Repositories;
 using MediatR;
 
@@ -9,18 +11,26 @@ namespace Kodlama.io.Devs.Application.Features.UserOperationClaims.Commands.Crea
     public class CreateUserOperationClaimCommandHandler : IRequestHandler<CreateUserOperationClaimCommandRequest, CreateUserOperationClaimCommandResponse>
     {
         private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+        private readonly IAppUserRepository _appUserRepository;
+        private readonly IOperationClaimRepository _operationClaimRepository;
         private readonly IMapper _mapper;
         private readonly RuleManager _ruleManager;
 
-        public CreateUserOperationClaimCommandHandler(IUserOperationClaimRepository userOperationClaimRepository, IMapper mapper, RuleManager ruleManager)
+        public CreateUserOperationClaimCommandHandler(IUserOperationClaimRepository userOperationClaimRepository, IMapper mapper, RuleManager ruleManager, IAppUserRepository appUserRepository, IOperationClaimRepository operationClaimRepository)
         {
             _userOperationClaimRepository = userOperationClaimRepository;
             _mapper = mapper;
             _ruleManager = ruleManager;
+            _appUserRepository = appUserRepository;
+            _operationClaimRepository = operationClaimRepository;
         }
 
         public async Task<CreateUserOperationClaimCommandResponse> Handle(CreateUserOperationClaimCommandRequest request, CancellationToken cancellationToken)
         {
+            await _ruleManager.CheckIfExistsAsync<UserNotFoundException>(operation: async () => await _appUserRepository.GetAsync(x => x.Id == request.UserId, tracking: false));
+
+            await _ruleManager.CheckIfExistsAsync<OperationClaimNotFoundException>(operation: async () => await _operationClaimRepository.GetAsync(x => x.Id == request.OperationClaimId, tracking: false));
+
             await _ruleManager.CheckIfAlreadyExistsAsync<UserOperationClaimAlreadyExistsException>(operation: async () => await _userOperationClaimRepository.GetAsync(x => x.UserId == request.UserId && x.OperationClaimId == request.OperationClaimId, tracking: false));
 
             UserOperationClaim userOperationClaim = _mapper.Map<UserOperationClaim>(request);
